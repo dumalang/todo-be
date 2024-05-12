d14662d70619b4d6fd16d11b112c50443f628a16
dumalang <jimmydumalang@gmail.com>
2024-05-12 23:57:02 +0700

    adjust appConfig, add security TOKEN env var as mandatory

--- src/config/app_config.ts
+++ src/config/app_config.ts
@@ -1,12 +1,26 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const appConfig = {
-  appName: process.env.APP_NAME,
-  debug: process.env.APP_DEBUG === 'true',
+interface IAppConfig {
+  appName: string;
+  debug: boolean;
+  appEnv: 'production' | 'development' | 'test' | string;
+  security: {
+    token: string;
+  };
+}
+
+if (!process.env.TOKEN) {
+  throw new Error('Mandatory env var TOKEN must be defined');
+}
+
+const appConfig: IAppConfig = {
+  appName: process.env.APP_NAME || 'APPNAME',
+  debug: process.env.APP_DEBUG === 'true' || false,
+  appEnv: process.env.APP_ENV || 'development',
   security: {
     token: process.env.TOKEN,
   },
 };
 
-export {appConfig};
+export {IAppConfig, appConfig};
