efff337e0ea0184ad10611794f293eb240b4dd3b
dumalang <jimmydumalang@gmail.com>
2024-05-12 23:57:02 +0700

    moved knex_config.ts as database_adapter.ts

--- src/adapters/database_adapter.ts
+++ src/adapters/database_adapter.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {knex} from 'knex';
-import * as connectionConfig from '../../../knexfile';
-import {appConfig} from '../app_config';
+import * as connectionConfig from '../../knexfile';
+import {appConfig} from '../config/app_config';
 
 const connection = connectionConfig.default[appConfig.appEnv];
 const DB = knex(connection);
 
-export {DB};
+export {DB, connection as databaseConnectionConfig};
